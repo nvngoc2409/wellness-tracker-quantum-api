e24818229eb34100c46e3b19f102be4a78c6309d
kevin <kevin@theprojectfactory.com>
2026-01-07 09:29:00 +0700

    Update metadata

--- src/routes/web.routes.js
+++ src/routes/web.routes.js
@@ -3,11 +3,11 @@ import express from "express";
 const router = express.Router();
 
 router.get("/privacy", (req, res) => {
-  res.render("privacy", { title: "Privacy Policy of WellQ app" });
+  res.render("privacy", { title: "Privacy Policy of Wellness Tracker ft Quantum" });
 });
 
 router.get("/terms", (req, res) => {
-  res.render("terms", { title: "Terms and Conditions of WellQ app" });
+  res.render("terms", { title: "Terms and Condition of Wellness Tracker ft Quantum" });
 });
 
 router.get("/contact-form", (req, res) => {
